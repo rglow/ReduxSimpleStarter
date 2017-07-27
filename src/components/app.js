4cbea7081a7ff09c511e251bbb889d92a5ed17b3
Rafal Glowacki <rafal.w.glowacki@gmail.com>
2017-07-27 22:23:37 +0200

    a work in progress

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
+import YTSearch from 'youtube-api-search';
 
-export default class App extends Component {
+const API_KEY = 'AIzaSyCnlNCj9EKnooQaIjCShv8Pjs0X8qO3Vzw';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'motorcycles'}, (videos) => this.setState({ videos }));
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
+      </div>
     );
   }
 }
+
+export default App;
